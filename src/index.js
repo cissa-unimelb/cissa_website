aabcf80bd8187c9434c0f32115a54813b0fd3b61
Andy Jiang <andypeiyuan@hotmail.com>
2020-04-15 21:32:25 +1000

    change strucutre for pm2

--- src/index.js
+++ src/index.js
@@ -6,28 +6,28 @@ var http = require('http').Server(app);
 var port = process.env.PORT || 4000;
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '../static/index.html');
+  res.sendFile(__dirname + '/static/index.html');
 });
 app.get('/events', function(req, res){
-  res.sendFile(__dirname + '../static/events.html');
+  res.sendFile(__dirname + '/static/events.html');
 });
 app.get('/about', function(req, res){
-  res.sendFile(__dirname + '../static/about.html');
+  res.sendFile(__dirname + '/static/about.html');
 });
 app.get('/gallery', function(req, res){
-  res.sendFile(__dirname + '../static/gallery.html');
+  res.sendFile(__dirname + '/static/gallery.html');
 });
 app.get('/past-events', function(req, res){
-  res.sendFile(__dirname + '../static/past_events.html');
+  res.sendFile(__dirname + '/static/past_events.html');
 });
 app.get('/contact', function(req, res){
-  res.sendFile(__dirname + '../static/contact.html');
+  res.sendFile(__dirname + '/static/contact.html');
 });
 app.get('/sponsors', function(req, res){
-  res.sendFile(__dirname + '../static/sponsors.html');
+  res.sendFile(__dirname + '/static/sponsors.html');
 });
 
-app.use('/', express.static('../static/'));
+app.use('/', express.static('static/'));
 
 http.listen(port, function(){
   console.log('listening on *:' + port);
