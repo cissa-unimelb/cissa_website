d45dc69b982555982c559980a22f6b60d5c9a502
thebowenfeng <857514.leofeng@gmail.com>
2022-09-29 22:56:27 +1000

    added sponsorship form

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 const nodemailer = require('nodemailer');
+const {google} = require('googleapis');
 
 var FB = require('fb');
 FB.options({version: 'v6.0'});
@@ -8,7 +9,54 @@ var app = express();
 var http = require('http').Server(app);
 var port = process.env.PORT || 4000;
 
+app.use(express.json());
+
 const access_token = 'EAADgzgjeDx8BANPAnK9BxkYsw07bPbkEwFBSwPVg159IL846dVHyZAoZA0auSZBLhcMsm1behnWxkFPAssLjKOGaeu4GuAhrPtCmZCxO5GyiiRU2b0VO9D9ActCv5b9v72gY2mRZCJujZCd9stUQ2mQ2ZClhOZAnSgCFr5Bxo0FMcQZDZD';
+
+const GMAIL_CLIENT_ID = "385637139395-pv6262mlqckc14egfshufjqpmgvqua8d.apps.googleusercontent.com"
+const GMAIL_CLIENT_SECRET = "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"
+const GMAIL_REDIRECT_URI = "https://developers.google.com/oauthplayground"
+const GMAIL_REFRESH_TOKEN = "1//04Eg_5tK9SjDVCgYIARAAGAQSNwF-L9IrpxKlR9v8o8ViUYIlT9qNecYmj1H3ld92qb2bzdVen879JULKuR9ynjExbvFTLDva9QI"
+
+const gmail_auth = {
+  type: "OAuth2",
+  user: "webmaster@cissa.org.au",
+  clientId: GMAIL_CLIENT_ID,
+  clientSecret: GMAIL_CLIENT_SECRET,
+  refreshToken: GMAIL_REFRESH_TOKEN
+}
+
+const oAuth2Client = new google.auth.OAuth2(GMAIL_CLIENT_ID, GMAIL_CLIENT_SECRET, GMAIL_REDIRECT_URI);
+oAuth2Client.setCredentials({refresh_token: GMAIL_REFRESH_TOKEN});
+
+app.post('/send-mail', async function(req, res){
+  try{
+    const access_token = oAuth2Client.getAccessToken();
+    const transport = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        ...gmail_auth,
+        accessToken: access_token
+      }
+    });
+
+    const mail_options = {
+      from: "webmaster@cissa.org.au",
+      to: req.body.receiver,
+      subject: req.body.subject,
+      text: req.body.text,
+    }
+
+    const result = await transport.sendMail(mail_options);
+
+    console.log(req.body);
+    //console.log(result);
+    res.send(result);
+  }catch(error){
+    console.log(error);
+    res.send({error: true});
+  }
+})
 
 app.get('/api-cj', function(req, res){
   FB.api(
